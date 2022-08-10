7d12083fd89a15f4e2773d336c729d8094237076
Julia Farias <julia.farias2000@hotmail.com>
2022-08-10 19:43:50 -0300

    :bug:(#11):Arrumando o css da header

--- desenvolvimento/DonAct-Front/src/App.js
+++ desenvolvimento/DonAct-Front/src/App.js
@@ -1,15 +1,26 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
-import Home from "../src/pages/Home"
-
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;

361db2e43c6ddf3897bf30ed4f739e90acd2e5bb
isadoragalvaoss <isadoragalvaoss@gmail.com>
2022-08-12 01:04:38 -0300

    :bulb:(#9): Adicionando rota de cadastro de doador
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- desenvolvimento/DonAct-Front/src/App.js
+++ desenvolvimento/DonAct-Front/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { BrowserRouter,Route,Routes } from "react-router-dom";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Home from "../src/pages/Home"
-import Login from "../src/pages/Login"
-import Choice from "../src/pages/Choice"
-import "./App.css"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
+import Choice from "../src/pages/Choice";
+import "./App.css";
+import SignUp from "./pages/SignUp";
 
 function App() {
-	return (
-		<BrowserRouter>
-     <div className="App">
-        <div className="outer">    
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="outer">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/choice" element={<Choice />} />
+            <Route path="/donor" element={<SignUp />} />
           </Routes>
         </div>
       </div>
     </BrowserRouter>
-	);
+  );
 }
 
 export default App;

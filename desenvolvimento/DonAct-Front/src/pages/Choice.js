a8b7291b89c120ded98cbdcab4c1e21f9710b9dd
isadoragalvaoss <isadoragalvaoss@gmail.com>
2022-08-12 01:03:19 -0300

    :racehorse:(#9): Trocando para arrow function
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- desenvolvimento/DonAct-Front/src/pages/Choice.js
+++ desenvolvimento/DonAct-Front/src/pages/Choice.js
@@ -1,38 +1,44 @@
-import React, { Component } from "react";
-import "../styles/choice.css"
+import React from "react";
+import "../styles/choice.css";
 import Logo from "../assets/logo.svg";
 
-export default class SignUp extends Component {
-    render() {
-        return (
-            <div className="inner inner-choice">
-                <div className="form">
-                    <div className="form-begin-choice">
-                        <div className="form-title">
-                            <img className="form-image" src={Logo} alt = "Donact"></img>
-                        </div>
-                        <div className="form-title">
-                            <h2 className="h2-choice">
-                            Como você gostaria de ajudar hoje?
-                            </h2>
-                        </div>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <div className="btn-duo">
-                            <div className="btn-tag ">
-                                <button type="submit" className="btn btn-dark btn-lg btn-block btn-primary btn-choice">Cadastrar ONG</button>
-                            </div>
-                            <div className="btn-tag">
-                                <div class="vl"></div>
-                            </div>
-                            <div className="btn-tag ">
-                                <button type="submit" className="btn btn-dark btn-lg btn-block btn-primary btn-choice">Sou um Doador</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+const Choice = () => (
+  <div className="inner inner-choice">
+    <div className="form">
+      <div className="form-begin-choice">
+        <div className="form-title">
+          <img className="form-image" src={Logo} alt="Donact"></img>
+        </div>
+        <div className="form-title">
+          <h2 className="h2-choice">Como você gostaria de ajudar hoje?</h2>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div className="btn-duo">
+          <div className="btn-tag ">
+            <button
+              type="submit"
+              className="btn btn-dark btn-lg btn-block btn-primary btn-choice"
+            >
+              Cadastrar ONG
+            </button>
+          </div>
+          <div className="btn-tag">
+            <div class="vl"></div>
+          </div>
+          <div className="btn-tag ">
+            <button
+              type="submit"
+              className="btn btn-dark btn-lg btn-block btn-primary btn-choice"
+            >
+              Sou um Doador
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Choice;
--- desenvolvimento/DonAct-Front/src/pages/Login.js
+++ desenvolvimento/DonAct-Front/src/pages/Login.js
@@ -1,42 +1,51 @@
-import React, { Component } from "react";
-import "../styles/login.css"
+import React from "react";
+import "../styles/login.css";
 import Logo from "../assets/logo.svg";
+import Input from "../components/Input";
+import Button from "../components/Button";
 
-export default class Login extends Component {
-    render() {
-        return (
-            
-            <div className="inner inner-login">
-                <div className="form">
-                    <form>
-                        <div className="form-title">
-                            <h2>
-                                <img className="form-image" src={Logo} alt = "Donact"></img>
-                            </h2>
-                        </div>
-                        <div className="form-begin">
-                            <div className="form-group">
-                                <label className="label-login"> CPF ou CNPJ</label>
-                                <input type="text" className="form-control" placeholder="CPF ou CNPJ" />
-                            </div>
-                            <br/>
-                            <div className="form-group">
-                                <label className="label-login"> Senha </label>
-                                <input type="password" className="form-control" placeholder="Insira sua senha" />
-                            </div>                
-                            <p className="forgot-password text-right">
-                                <a href="http://google.com">esqueceu a senha?</a>
-                            </p>
-                        </div>
-                        <div className="form-btn center justify-content-center">
-                            <button type="submit" className="btn btn-dark btn-lg btn-block btn-primary">Entrar</button>
-                        </div>
-                    </form>
-                    <p className="register-text text-center ">
-                        não possui conta? <a href="/choice">clique aqui</a>
-                    </p>
-                </div>
-            </div>
-        );
-    }
-}
+const Login = () => (
+  <div className="inner inner-login">
+    <div className="form">
+      <form>
+        <div className="form-title">
+          <h2>
+            <img className="form-image" src={Logo} alt="Donact"></img>
+          </h2>
+        </div>
+        <div className="form-begin">
+          <div className="form-group">
+            <Input
+              classLabel="label-login"
+              text="CPF ou CNPJ"
+              type="text"
+              placeholder="CPF ou CNPJ"
+              classInput="form-control"
+            />
+          </div>
+          <br />
+          <div className="form-group">
+            <Input
+              classLabel="label-login"
+              text="Senha"
+              type="password"
+              placeholder="Insira sua senha"
+              classInput="form-control"
+            />
+          </div>
+          <p className="forgot-password text-right">
+            <a href="http://google.com">esqueceu a senha?</a>
+          </p>
+        </div>
+        <div className="form-btn center justify-content-center">
+          <Button text="Entrar" />
+        </div>
+      </form>
+      <p className="register-text text-center ">
+        não possui conta? <a href="/choice">clique aqui</a>
+      </p>
+    </div>
+  </div>
+);
+
+export default Login;

f421eb19f350bfba3fdf4d4fcd7a19a76f04d733
antoniotoineto <netocaastro10@gmail.com>
2022-08-24 15:19:10 -0300

    :repeat:(#11) Adicionando alterações requisitadas no PR do Eduardo

--- desenvolvimento/DonAct-API/src/controllers/ONGController.js
+++ desenvolvimento/DonAct-API/src/controllers/ONGController.js
@@ -5,87 +5,59 @@ module.exports = {
       res.send("Página Inicial")
     }, 
     cadastrar: async (req, res) => {
-      const select = await Ong.findByPk(req.body.cnpj)
+      const selectCNPJ = await Ong.findByPk(req.body.cnpj)
+      const selectTel = await Ong.findOne({ where: { telefone: req.body.telefone } })
+      const selectEmail = await Ong.findOne({ where: { email: req.body.email } })
+      const selectEmailResp = await Ong.findOne({ where: { emailDoResponsavel: req.body.emailDoResponsavel } })
+      //const validaDados = require("../utils/utils")
 
-      switch(validaDados(req.body)){
-        case 1:
-          res.send("Cnpj inválido. Digite apenas 14 números.")
-          break
-        case 2:
-          res.send("Nome inválido. Digite somente letras.")
-          break
-        case 3:
-          res.send("Telefone inválido. Digite apenas 11 números.")
-          break
-        case 4:
-          res.send("Cidade inválida. Digite somente letras.")
-          break
-        case 5:
-          res.send("Estado inválido. Digite somente letras.")
-          break
-        case 6:
-          res.send("CEP inválido. Digite apenas 8 números.")
-          break
-        case 7:
-          res.send("Nome do responsável inválido. Digite somente letras.")
-          break
-        //case 8:
-        //  res.send("Email inválido.")
-        //  break
-        case 9:
-          if (select == null){
-            await Ong.create(req.body) 
-            res.send("Cadastrado com sucesso!")
-            break
-          } else {
-            res.send("Esse CNPJ já está cadastrado.")
-            break
-          }
-        default:
-          res.send("Não foi possível completar o cadastro.")
-          break
-      }
-      
+      if(validaDados(req.body, res) == true){
+        if(selectCNPJ == null){
+          if(selectTel == null){
+            if(selectEmail == null){
+              if(selectEmailResp == null){
+                await Ong.create(req.body) 
+                res.send("Cadastrado com sucesso!")
+
+              } else { res.send("Esse email de responsável já está cadastrado.") }
+            } else { res.send("Esse email já está cadastrado.") }
+          } else { res.send("Esse telefone já está cadastrado.") }
+        } else { res.send("Esse CNPJ já está cadastrado.") }
+      } 
+
     },
     editar: async (req, res) => {
-      const select = await Ong.findByPk(req.params.id)
-      if(select != null) { 
-        await Ong.update(
+      const selectCNPJ = await Ong.findByPk(req.params.id)
+      //const validaDados = require("../utils/utils")
+
+      if(selectCNPJ != null) { 
+        if (validaDados(req.body, res) == true){
+          await Ong.update(
+            {
+            "cnpj": req.body.cnpj,
+            "fotoDePerfil": req.body.fotoDePerfil,
+            "nome": req.body.nome,
+            "telefone": req.body.telefone,
+            "email": req.body.email,
+            "endereco": req.body.endereco,
+            "cidade": req.body.cidade,
+            "estado": req.body.estado,
+            "cep": req.body.cep,
+            "senha": req.body.senha,
+            "nomeDoResponsavel": req.body.nomeDoResponsavel,
+            "emailDoResponsavel": req.body.emailDoResponsavel
+          },
           {
-          "cnpj": req.body.cnpj,
-          "fotoDePerfil": req.body.fotoDePerfil,
-          "nome": req.body.nome,
-          "telefone": req.body.telefone,
-          "email": req.body.email,
-          "endereco": req.body.endereco,
-          "cidade": req.body.cidade,
-          "estado": req.body.estado,
-          "cep": req.body.cep,
-          "senha": req.body.senha,
-          "nomeDoResponsavel": req.body.nomeDoResponsavel,
-          "emailDoResponsavel": req.body.emailDoResponsavel
-        },
-        {
-          where: {"cnpj": req.params.id},
-          truncate: false,
-        });
-        res.send("Usuário editado com sucesso!")
+            where: {"cnpj": req.params.id},
+            truncate: false,
+          });
+          res.send("Usuário editado com sucesso!")
+        }
       }
       else
         res.send("Usuário não encontrado!")
     },
     mostrar: async (req, res) => {
-      console.log(req.body.cnpj)
-      console.log("cpnj: "+typeof(req.body.cnpj))
-      console.log("nome: "+typeof(req.body.nome))
-      console.log("email: "+typeof(req.body.email))
-      console.log("telefone: "+typeof(req.body.telefone))
-      console.log("cidade: "+typeof(req.body.cidade))
-      console.log("estado: "+typeof(req.body.estado))
-      console.log("cep: "+typeof(req.body.cep))
-      console.log("nome resp: "+typeof(req.body.nomeDoResponsavel))
-      console.log("email resp: "+typeof(req.body.emailDoResponsavel))
-
       const users = await Ong.findAll();
       res.send(users);
     },
@@ -102,8 +74,7 @@ module.exports = {
     }
   }
 
-function validaDados(object, res){
-  console.log(typeof(res))
+function validaDados(object, resp){
   if(object.cnpj.toString().length == 14) {
     if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.nome) == true){
       if(/^[0-9]+$/.test(object.telefone) && object.telefone.toString().length == 11){
@@ -112,13 +83,34 @@ function validaDados(object, res){
             if(/^[0-9]+$/.test(object.cep) && object.cep.toString().length == 8){
               if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.nomeDoResponsavel) == true){
                 //if(/^[a-z0-9@]$/.test(object.emailDoResponsavel) == true) {
-                  return 9
-                //} else { return 8 }
-              } else { return 7 }
-            } else { return 6 }
-          } else { return 5 }
-        } else { return 4 }
-      } else { return 3 }
-    } else { return 2 }
-  } else { return 1 }
-}
+                  return true
+                //}
+              } else { 
+                resp.send("Nome do responsável inválido. Digite somente letras.")
+                return false
+              }
+            } else { 
+              resp.send("CEP inválido. Digite apenas 8 números.")
+              return false
+            }
+          } else { 
+            resp.send("Estado inválido. Digite somente letras.")
+            return false
+          }
+        } else { 
+          resp.send("Cidade inválida. Digite somente letras.")
+          return false
+        }
+      } else { 
+        resp.send("Telefone inválido. Digite apenas 11 números.")
+        return false
+      }
+    } else { 
+      resp.send("Nome inválido. Digite somente letras.")
+      return false
+    }
+  } else { 
+    resp.send("Cnpj inválido. Digite apenas 14 números.")  
+    return false
+  }
+} 

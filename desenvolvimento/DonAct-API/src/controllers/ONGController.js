b1a948efe45562d5a2f468b195b23f0419be46d4
antoniotoineto <netocaastro10@gmail.com>
2022-08-24 01:16:25 -0300

    :bulb: (#11) Adicionando validações de entradas

--- desenvolvimento/DonAct-API/src/controllers/ONGController.js
+++ desenvolvimento/DonAct-API/src/controllers/ONGController.js
@@ -3,15 +3,49 @@ const Ong = require("../models/ONGModel")
 module.exports = {
     inicial: async (req, res) => {
       res.send("Página Inicial")
-    },
+    }, 
     cadastrar: async (req, res) => {
       const select = await Ong.findByPk(req.body.cnpj)
-      if(select == null) {
-        await Ong.create(req.body) 
-        res.send("Cadastrado com sucesso!")
+
+      switch(validaDados(req.body)){
+        case 1:
+          res.send("Cnpj inválido. Digite apenas 14 números.")
+          break
+        case 2:
+          res.send("Nome inválido. Digite somente letras.")
+          break
+        case 3:
+          res.send("Telefone inválido. Digite apenas 11 números.")
+          break
+        case 4:
+          res.send("Cidade inválida. Digite somente letras.")
+          break
+        case 5:
+          res.send("Estado inválido. Digite somente letras.")
+          break
+        case 6:
+          res.send("CEP inválido. Digite apenas 8 números.")
+          break
+        case 7:
+          res.send("Nome do responsável inválido. Digite somente letras.")
+          break
+        //case 8:
+        //  res.send("Email inválido.")
+        //  break
+        case 9:
+          if (select == null){
+            await Ong.create(req.body) 
+            res.send("Cadastrado com sucesso!")
+            break
+          } else {
+            res.send("Esse CNPJ já está cadastrado.")
+            break
+          }
+        default:
+          res.send("Não foi possível completar o cadastro.")
+          break
       }
-      else
-        res.send("Já foi cadastrado um usuário com esse cnpj!")
+      
     },
     editar: async (req, res) => {
       const select = await Ong.findByPk(req.params.id)
@@ -41,6 +75,17 @@ module.exports = {
         res.send("Usuário não encontrado!")
     },
     mostrar: async (req, res) => {
+      console.log(req.body.cnpj)
+      console.log("cpnj: "+typeof(req.body.cnpj))
+      console.log("nome: "+typeof(req.body.nome))
+      console.log("email: "+typeof(req.body.email))
+      console.log("telefone: "+typeof(req.body.telefone))
+      console.log("cidade: "+typeof(req.body.cidade))
+      console.log("estado: "+typeof(req.body.estado))
+      console.log("cep: "+typeof(req.body.cep))
+      console.log("nome resp: "+typeof(req.body.nomeDoResponsavel))
+      console.log("email resp: "+typeof(req.body.emailDoResponsavel))
+
       const users = await Ong.findAll();
       res.send(users);
     },
@@ -56,3 +101,24 @@ module.exports = {
         res.send("Usuário não encontrado!")
     }
   }
+
+function validaDados(object, res){
+  console.log(typeof(res))
+  if(object.cnpj.toString().length == 14) {
+    if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.nome) == true){
+      if(/^[0-9]+$/.test(object.telefone) && object.telefone.toString().length == 11){
+        if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.cidade) == true){
+          if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.estado) == true){
+            if(/^[0-9]+$/.test(object.cep) && object.cep.toString().length == 8){
+              if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.nomeDoResponsavel) == true){
+                //if(/^[a-z0-9@]$/.test(object.emailDoResponsavel) == true) {
+                  return 9
+                //} else { return 8 }
+              } else { return 7 }
+            } else { return 6 }
+          } else { return 5 }
+        } else { return 4 }
+      } else { return 3 }
+    } else { return 2 }
+  } else { return 1 }
+}

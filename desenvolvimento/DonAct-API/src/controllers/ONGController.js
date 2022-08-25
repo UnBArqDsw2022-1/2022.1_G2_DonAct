b6220a931997d8beae3d57020b2bb54460baa0d1
Victor Eduardo <victorear05@gmail.com>
2022-08-25 10:08:15 -0300

    :repeat:(#11) Finalizando validações de dados para cadastro ou edição

--- desenvolvimento/DonAct-API/src/controllers/ONGController.js
+++ desenvolvimento/DonAct-API/src/controllers/ONGController.js
@@ -1,116 +1,92 @@
 const Ong = require("../models/ONGModel")
+const validaDados = require("../utils/utils")
 
 module.exports = {
-    inicial: async (req, res) => {
-      res.send("Página Inicial")
-    }, 
-    cadastrar: async (req, res) => {
-      const selectCNPJ = await Ong.findByPk(req.body.cnpj)
-      const selectTel = await Ong.findOne({ where: { telefone: req.body.telefone } })
-      const selectEmail = await Ong.findOne({ where: { email: req.body.email } })
-      const selectEmailResp = await Ong.findOne({ where: { emailDoResponsavel: req.body.emailDoResponsavel } })
-      //const validaDados = require("../utils/utils")
+  inicial: async (req, res) => {
+    res.send("Página Inicial")
+  },
+  cadastrar: async (req, res) => {
+    const selectCNPJ = await Ong.findByPk(req.body.cnpj)
+    const selectTel = await Ong.findOne({ where: { telefone: req.body.telefone } })
+    const selectEmail = await Ong.findOne({ where: { email: req.body.email } })
+    const selectEmailResp = await Ong.findOne({ where: { emailDoResponsavel: req.body.emailDoResponsavel } })
 
-      if(validaDados(req.body, res) == true){
-        if(selectCNPJ == null){
-          if(selectTel == null){
-            if(selectEmail == null){
-              if(selectEmailResp == null){
-                await Ong.create(req.body) 
-                res.send("Cadastrado com sucesso!")
-
-              } else { res.send("Esse email de responsável já está cadastrado.") }
-            } else { res.send("Esse email já está cadastrado.") }
-          } else { res.send("Esse telefone já está cadastrado.") }
-        } else { res.send("Esse CNPJ já está cadastrado.") }
-      } 
-
-    },
-    editar: async (req, res) => {
-      const selectCNPJ = await Ong.findByPk(req.params.id)
-      //const validaDados = require("../utils/utils")
-
-      if(selectCNPJ != null) { 
-        if (validaDados(req.body, res) == true){
-          await Ong.update(
-            {
-            "cnpj": req.body.cnpj,
-            "fotoDePerfil": req.body.fotoDePerfil,
-            "nome": req.body.nome,
-            "telefone": req.body.telefone,
-            "email": req.body.email,
-            "endereco": req.body.endereco,
-            "cidade": req.body.cidade,
-            "estado": req.body.estado,
-            "cep": req.body.cep,
-            "senha": req.body.senha,
-            "nomeDoResponsavel": req.body.nomeDoResponsavel,
-            "emailDoResponsavel": req.body.emailDoResponsavel
-          },
-          {
-            where: {"cnpj": req.params.id},
-            truncate: false,
-          });
-          res.send("Usuário editado com sucesso!")
-        }
-      }
-      else
-        res.send("Usuário não encontrado!")
-    },
-    mostrar: async (req, res) => {
-      const users = await Ong.findAll();
-      res.send(users);
-    },
-    deletar: async(req, res) => {
-      const select = await Ong.findByPk(req.params.id)
-      if(select != null) {
-        await Ong.destroy({
-          where: {"cnpj":req.params.id},
-          truncate: false,
-        }, res.send("Deletado com sucesso"))
-      }
-      else
-        res.send("Usuário não encontrado!")
+    if (validaDados(req.body, res) == true) {
+      if (selectCNPJ == null) {
+        if (selectTel == null) {
+          if (selectEmail == null) {
+            if (selectEmailResp == null) {
+              await Ong.create(req.body)
+              res.send("Cadastrado com sucesso!")
+            } else { res.send("Esse email de responsável já está cadastrado.") }
+          } else { res.send("Esse email já está cadastrado.") }
+        } else { res.send("Esse telefone já está cadastrado.") }
+      } else { res.send("Esse CNPJ já está cadastrado.") }
     }
-  }
-
-function validaDados(object, resp){
-  if(object.cnpj.toString().length == 14) {
-    if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.nome) == true){
-      if(/^[0-9]+$/.test(object.telefone) && object.telefone.toString().length == 11){
-        if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.cidade) == true){
-          if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.estado) == true){
-            if(/^[0-9]+$/.test(object.cep) && object.cep.toString().length == 8){
-              if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/.test(object.nomeDoResponsavel) == true){
-                //if(/^[a-z0-9@]$/.test(object.emailDoResponsavel) == true) {
-                  return true
-                //}
-              } else { 
-                resp.send("Nome do responsável inválido. Digite somente letras.")
-                return false
+  },
+  editar: async (req, res) => {
+    const selectCNPJ = await Ong.findByPk(req.params.id)
+    const selectTel = await Ong.findOne({ where: { telefone: req.body.telefone }})
+    const selectEmail = await Ong.findOne({ where: { email: req.body.email } })
+    const selectEmailResp = await Ong.findOne({ where: { emailDoResponsavel: req.body.emailDoResponsavel } })
+    if (selectCNPJ != null) {
+      if (validaDados(req.body, res) == true) {
+        if (req.params.id == req.body.cnpj || selectCNPJ == null) {
+          if (selectCNPJ.telefone == req.body.telefone || selectTel == null ) {
+            if (selectCNPJ.email == req.body.email || selectEmail == null) {
+              if (selectCNPJ.emailDoResponsavel == req.body.emailDoResponsavel || selectEmailResp == null) {
+                await Ong.update({
+                  "cnpj": req.body.cnpj,
+                  "fotoDePerfil": req.body.fotoDePerfil,
+                  "nome": req.body.nome,
+                  "telefone": req.body.telefone,
+                  "email": req.body.email,
+                  "endereco": req.body.endereco,
+                  "cidade": req.body.cidade,
+                  "estado": req.body.estado,
+                  "cep": req.body.cep,
+                  "senha": req.body.senha,
+                  "nomeDoResponsavel": req.body.nomeDoResponsavel,
+                  "emailDoResponsavel": req.body.emailDoResponsavel
+                },
+                  {
+                    where: { "cnpj": req.params.id },
+                    truncate: false,
+                  });
+                res.send("Usuário editado com sucesso!")
               }
-            } else { 
-              resp.send("CEP inválido. Digite apenas 8 números.")
-              return false
+              else {
+                res.send("Esse email de responsável já está cadastrado.")
+              }
             }
-          } else { 
-            resp.send("Estado inválido. Digite somente letras.")
-            return false
+            else {
+              res.send("Esse email de ONG já está cadastrado.")
+            }
           }
-        } else { 
-          resp.send("Cidade inválida. Digite somente letras.")
-          return false
+          else {
+            res.send("Esse telefone já está cadastrado.")
+          }
+        } else {
+          res.send("CNPJ já cadastrado!")
         }
-      } else { 
-        resp.send("Telefone inválido. Digite apenas 11 números.")
-        return false
       }
-    } else { 
-      resp.send("Nome inválido. Digite somente letras.")
-      return false
+      else
+        res.send("Usuário não encontrado!")
     }
-  } else { 
-    resp.send("Cnpj inválido. Digite apenas 14 números.")  
-    return false
+  },
+  mostrar: async (req, res) => {
+    const users = await Ong.findAll();
+    res.send(users);
+  },
+  deletar: async (req, res) => {
+    const select = await Ong.findByPk(req.params.id)
+    if (select != null) {
+      await Ong.destroy({
+        where: { "cnpj": req.params.id },
+        truncate: false,
+      }, res.send("Deletado com sucesso"))
+    }
+    else
+      res.send("Usuário não encontrado!")
   }
-} 
+}

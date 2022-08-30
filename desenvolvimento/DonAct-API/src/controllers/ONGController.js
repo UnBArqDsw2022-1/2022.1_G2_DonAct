85c69e580dffc1c52399dd3a4f2b706e34be77a3
Victor Eduardo <victorear05@gmail.com>
2022-08-30 00:23:28 -0300

    :repeat:(#3): Modificando Validações

--- desenvolvimento/DonAct-API/src/controllers/ONGController.js
+++ desenvolvimento/DonAct-API/src/controllers/ONGController.js
@@ -1,63 +1,80 @@
 const axios = require('axios').default;
 const Ong = require("../models/ONGModel")
 
+
 module.exports = {
   inicial: async (req, res) => {
     res.send("Página Inicial")
   },
   cadastrar: async (req, res) => { 
-    try {
-      const receitaFederal = await axios.get(`https://receitaws.com.br/v1/cnpj/${req.body.cnpj}`);
-      const cnpjValido = {
-        "status": receitaFederal.data.status,
-        "nome": receitaFederal.data.nome,
-        "capital_social": Number(receitaFederal.data.capital_social)
-      }
-      if (cnpjValido.status == "ERROR") {
-        res.send("O CNPJ inserido não está na Receita Federal!")
-      }
-      else if (cnpjValido.capital_social != 0)
-        res.send("A instituição possui fins lucrativos!")
-      else (select == null) {
-        await Ong.create(req.body)
-        .then(function() {
-          res.send("Ong cadastrada com sucesso!")
-        })
-        .catch(function(err) {
-          res.send(err)
-        })
-      }
+    let CNPJvalido = true
+    const receitaFederal = await axios.get(`https://receitaws.com.br/v1/cnpj/${req.body.cnpj}`);
+    const cnpjValido = {
+      "status": receitaFederal.data.status,
+      "nome": receitaFederal.data.nome,
+      "capital_social": Number(receitaFederal.data.capital_social)
     }
-    catch (e) {
-      res.send("Erro na validação do CNPJ!");
+    if (cnpjValido.status == "ERROR") {
+      res.send("O CNPJ inserido não está na Receita Federal!")
+      CNPJvalido = false
     }
-  },
-  editar: async (req, res) => {
-    await Ong.update({
-      "cnpj": req.body.cnpj,
-      "fotoDePerfil": req.body.fotoDePerfil,
-      "nome": req.body.nome,
-      "telefone": req.body.telefone,
-      "email": req.body.email,
-      "endereco": req.body.endereco,
-      "cidade": req.body.cidade,
-      "estado": req.body.estado,
-      "cep": req.body.cep,
-      "senha": req.body.senha,
-      "nomeDoResponsavel": req.body.nomeDoResponsavel,
-      "emailDoResponsavel": req.body.emailDoResponsavel
-    },
-      {
-        where: { "cnpj": req.params.id },
-        truncate: false,
-      })
+    else if (cnpjValido.capital_social != 0) {
+      res.send("A instituição possui fins lucrativos!")
+      CNPJvalido = false
+    }
+      
+    if (CNPJvalido == true) {
+      await Ong.create(req.body)
       .then(function() {
-        res.send("Usuário editado com sucesso!")
+        res.send("Ong cadastrada com sucesso!")
       })
       .catch(function(err) {
         res.send(err)
       })
-    
+    }
+  },
+  editar: async (req, res) => {
+    let CNPJvalido = true
+    const receitaFederal = await axios.get(`https://receitaws.com.br/v1/cnpj/${req.body.cnpj}`);
+    const cnpjValido = {
+      "status": receitaFederal.data.status,
+      "nome": receitaFederal.data.nome,
+      "capital_social": Number(receitaFederal.data.capital_social)
+    }
+    if (cnpjValido.status == "ERROR") {
+      res.send("O CNPJ inserido não está na Receita Federal!")
+      CNPJvalido = false
+    }
+    else if (cnpjValido.capital_social != 0) {
+      res.send("A instituição possui fins lucrativos!")
+      CNPJvalido = false
+    }
+    if (CNPJvalido) {
+      await Ong.update({
+        "cnpj": req.body.cnpj,
+        "fotoDePerfil": req.body.fotoDePerfil,
+        "nome": req.body.nome,
+        "telefone": req.body.telefone,
+        "email": req.body.email,
+        "endereco": req.body.endereco,
+        "cidade": req.body.cidade,
+        "estado": req.body.estado,
+        "cep": req.body.cep,
+        "senha": req.body.senha,
+        "nomeDoResponsavel": req.body.nomeDoResponsavel,
+        "emailDoResponsavel": req.body.emailDoResponsavel
+      },
+        {
+          where: { "cnpj": req.params.id },
+          truncate: false,
+        })
+        .then(function() {
+          res.send("Usuário editado com sucesso!")
+        })
+        .catch(function(err) {
+          res.send(err)
+        })
+    }
   },
   mostrar: async (req, res) => {
     const users = await Ong.findAll();

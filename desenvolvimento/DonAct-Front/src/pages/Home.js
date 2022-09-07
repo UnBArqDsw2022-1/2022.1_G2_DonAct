f1d5231edf1f03a653396f211ad4964662d9a817
Felipe Agustini <felipeboccardi@hotmail.com>
2022-09-06 22:35:03 -0300

    #26 Adicionando Componente de Navbar

--- desenvolvimento/DonAct-Front/src/pages/Home.js
+++ desenvolvimento/DonAct-Front/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import HeaderSimple from "../components/HeaderSimple";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Home = (props) => {
     return (
         <div id="body-container">
-
+            <Navbar/>
             <HeaderSimple></HeaderSimple>
             <div id="footer">
             <Footer></Footer>

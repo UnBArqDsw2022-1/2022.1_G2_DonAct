02cd3979fca76b23a1bb21bd517255d5f5109254
Felipe Agustini <felipeboccardi@hotmail.com>
2022-09-07 15:51:53 -0300

    Tela de home

--- desenvolvimento/DonAct-Front/src/pages/Home.js
+++ desenvolvimento/DonAct-Front/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import HeaderSimple from "../components/HeaderSimple";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
@@ -7,10 +7,7 @@ const Home = (props) => {
     return (
         <div id="body-container">
             <Navbar/>
-            <HeaderSimple></HeaderSimple>
-            <div id="footer">
-            <Footer></Footer>
-            </div>
+            <Header/>
 	    </div>
     );
 

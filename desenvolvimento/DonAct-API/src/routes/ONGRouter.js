6e1a758d52a82968241f015600560c31dd72f4b4
Victor Eduardo <victorear05@gmail.com>
2022-09-09 01:10:47 -0300

    :bulb:(#17): Adicionando funcionalidade de visualização de Itens
    Co-authored-by: Luiz Henrique <luizhemrique.fnandes@gmail.com>

--- desenvolvimento/DonAct-API/src/routes/ONGRouter.js
+++ desenvolvimento/DonAct-API/src/routes/ONGRouter.js
@@ -5,6 +5,7 @@ const ONGController = require("../controllers/ONGController")
 router.get("/", ONGController.inicial)
 router.post("/cadastrar", ONGController.cadastrar)
 router.get("/:id/editar", ONGController.editar)
+router.get("/:id/itens", ONGController.itens)
 router.get("/mostrar", ONGController.mostrar)
 router.delete("/:id/excluir", ONGController.deletar)
 
--- desenvolvimento/DonAct-API/src/server.js
+++ desenvolvimento/DonAct-API/src/server.js
@@ -10,7 +10,7 @@ configurar.relacionamentos()
 configurar.sincronizarModel()
 rotas.iniciarRotas(app)
 
-const port = process.env.PORT || 8080
+const port = process.env.DB_PORT || 8080
 
 app.listen(port, function () {
   console.log("server listening on port 8080")

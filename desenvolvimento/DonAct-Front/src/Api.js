4fe936dc058bc5764886c5104b20ce104690091e
Felipe Agustini <felipeboccardi@hotmail.com>
2022-09-09 20:23:05 -0300

    Arrumando rotas
    Co-authored-by: Victor Buendia <victorbuendia03@gmail.com>

--- desenvolvimento/DonAct-Front/src/Api.js
+++ desenvolvimento/DonAct-Front/src/Api.js
@@ -1,81 +1,87 @@
 export async function handlePostDoador(params) {
-    try {
-      const response = await fetch('http://localhost:8080/doador/cadastrar', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: {fotoDePerfil: params.fotoDePerfil,
-               nome: params.nome,
-                telefone: params.telefone,
-                email: params.email,
-                endereco: params.endereco,
-                cidade: params.cidade,
-                estado: params.estado,
-                senha: params.senha,
-                cpf: params.cpf,
-                dataNascimento: params.cpf}
-      })
-  
-      const data = await response.json()
-      
-      return data
-    } catch (error) {
-      throw error
-    }
-  }
-  
-  export async function handleGetDoador(id) {
-    try {
-      const response = await fetch(`http://localhost:8080/doador/encontrar/${id}`)
-  
-      const data = await response.json()
-      
-      return data
-    } catch (error) {
-      throw error
-    }
-  }
-  export async function handlePostONG(params) {
-    try {
-      const response = await fetch('http://localhost:8080/ong/cadastrar', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: {fotoDePerfil: params.fotoDePerfil,
-          cnpj: params.cnpj,
-          nome: params.nome,
-          telefone: params.telefone,
-          email: params.email,
-          endereco: params.endereco,
-          cidade: params.cidade,
-          estado: params.estado,
-          cep: params.cep,
-          senha: params.senha,
-          nomeDoResponsavel: params.nomeDoResponsavel,
-          emailDoResponsavel: params.emailDoResponsavel,}
-      })
-  
-      const data = await response.json()
-      
-      return data
-    } catch (error) {
-      throw error
-    }
-  }
+  try {
+    const response = await fetch('http://localhost:8080/doador/cadastrar', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        fotoDePerfil: null,
+        nome: params.nome,
+        telefone: params.telefone,
+        email: params.email,
+        endereco: params.endereco,
+        cidade: params.cidade,
+        estado: params.estado,
+        senha: params.senha,
+        cpf: params.cpf,
+        dataNascimento: params.dataNascimento
+      }, null, 2)
+    })
 
+    const data = await response.json();
 
-  export async function handleGetDoadorAll(id) {
-    try {
-      const response = await fetch('http://localhost:8080/doador/mostrar')
-  
-      const data = await response.json()
-      
-      return data
-    } catch (error) {
-      throw error
-    }
+    return data
+  } catch (error) {
+    throw error
   }
+}
 
+export async function handlePostONG(params) {
+  const body = {
+    fotoDePerfil: params.fotoDePerfil,
+    cnpj: params.cnpj,
+    nome: params.nome,
+    telefone: params.telefone,
+    email: params.email,
+    endereco: params.endereco,
+    cidade: params.cidade,
+    estado: params.estado,
+    cep: params.cep,
+    senha: params.senha,
+    nomeDoResponsavel: params.nomeDoResponsavel,
+    emailDoResponsavel: params.emailDoResponsavel,
+  };
 
+  try {
+    const response = await fetch('http://localhost:8080/ong/cadastrar', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function handleGetDoador(id) {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/doador/encontrar/${id}`
+    );
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function handleGetDoadorAll(id) {
+  try {
+    const response = await fetch("http://localhost:8080/doador/mostrar");
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}

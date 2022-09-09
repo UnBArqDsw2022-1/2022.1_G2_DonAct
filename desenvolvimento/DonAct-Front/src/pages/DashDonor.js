ee91098722c9b3c211ce31395e6ff17e5c238cf5
Felipe Agustini <felipeboccardi@hotmail.com>
2022-09-08 22:01:55 -0300

    Adicionando previa das ongs

--- desenvolvimento/DonAct-Front/src/pages/DashDonor.js
+++ desenvolvimento/DonAct-Front/src/pages/DashDonor.js
@@ -2,7 +2,52 @@ import React from "react";
 import Search from "../components/Search";
 import Navbar from "../components/Navbar";
 import "../styles/dash.css";
+import placehdfr from "../assets/ong-ubuntu-brasilia.jpg"
 const DashDonor = (props) => {
+    function returnlist(item){
+        return(
+            <li>
+                {item}
+            </li>
+        );
+    };
+    const array=[{
+        name: "Santuario dos gnomos",
+        image: {placehdfr},
+        need: ["roupas","cobertores","Toalhas"]
+
+    },{
+        name: "Santuario dos gnomos",
+        image: "imagem",
+        need: ["roupas","cobertores","Toalhas"]
+
+    },{
+        name: "Santuario dos gnomos",
+        image: "imagem",
+        need: ["roupas","cobertores","Toalhas"]
+
+    }];
+    
+    const ongbox = array.map((element) =>
+        <div className="ong-box col-md-12">
+            <div className="col-md-3">
+                <img src={element.image} className="box-image" />
+            </div>
+            <div className="col-md-9">
+                <span className="ong-title">
+                    {element.name}
+                </span>
+                <div className="bar-divider-box"/>
+                Precisamos de:
+                <ul>
+                    {element.need.forEach(returnlist)}
+                </ul>
+                
+            </div>
+        </div>
+    );
+    
+  
     return (
         <div id="body-container">
             <Navbar/>
@@ -28,6 +73,9 @@ const DashDonor = (props) => {
                         </div>
                         <br></br>
                         <div className="bar-divider-body"></div>
+                        <div>
+                                {ongbox}
+                        </div>
                     </div>
             </div>
 	    </div>
